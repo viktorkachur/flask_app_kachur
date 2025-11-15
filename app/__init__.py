51cd5e6fdc2f8e2e9b5a12a0a26c1959d6dbd074
Viktor Kachur <viktor.kachur.23@pnu.edu.ua>
2025-11-15 16:44:19 +0200

    Lab 3: Added 'users' Blueprint with routes and templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 application = Flask(__name__)
 
 application.config.from_object('config.AppConfig')
+from app.users import users_bp
 
+application.register_blueprint(users_bp, url_prefix='/users')
 
-from app import routes
+from app import views
---
+++ app/users/views.py
@@ -0,0 +1,22 @@
+
+from flask import render_template, request, url_for, redirect
+from app.users import users_bp
+
+
+@users_bp.route("/hi/<string:name>")
+def greetings(name):
+
+    age = request.args.get("age", None)
+
+    processed_name = name.title()
+
+    return render_template("users/hi.html",
+                           name=processed_name,
+                           age=age)
+
+
+@users_bp.route("/admin")
+def admin():
+
+    to_url = url_for("users.greetings", name="Supervisor", age=99)
+    return redirect(to_url)
