ec307fb7c36729061fb3f7f200c3ffd3165da71a
Viktor Kachur <viktor.kachur.23@pnu.edu.ua>
2025-11-15 16:57:23 +0200

    Lab 3: Added 'products' blueprint tests and completed all tasks

--- app/users/views.py
+++ app/users/views.py
@@ -2,21 +2,19 @@
 from flask import render_template, request, url_for, redirect
 from app.users import users_bp
 
-
 @users_bp.route("/hi/<string:name>")
 def greetings(name):
-
     age = request.args.get("age", None)
 
-    processed_name = name.title()
+
+    processed_name = name.upper()
 
     return render_template("users/hi.html",
                            name=processed_name,
                            age=age)
 
-
 @users_bp.route("/admin")
 def admin():
 
-    to_url = url_for("users.greetings", name="Supervisor", age=99)
+    to_url = url_for("users.greetings", name="administrator", age=45)
     return redirect(to_url)
